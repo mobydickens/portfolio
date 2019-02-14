c437e9fe223e8fda7c11fdc4e73f4ee613405d47
mobydickens <steadmegan@gmail.com>
2019-02-14 14:38:36 -0700

    Header work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { withRouter } from 'react-router-dom';
 
 const StyledHeader = styled.header`
   display: flex;
@@ -7,24 +8,64 @@ const StyledHeader = styled.header`
   height: 60px;
   background-color: black;
 `
-
+// bars are white when menu is closed, grey when open
 const NavBars = styled.i`
-  color: white;
-  font-size: 30px;
+  color: ${props => props.inputColor};
+  font-size: 24px;
   padding: 15px;
 `
+const Drop = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 50vh;
+  background-color: #333333;
+`
+const MenuItems = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20%;
+  border-bottom: 1px solid rgb(73, 73, 73);
+  color: white;
+  font-size: 30px;
+`
 
-const Name = styled(NavBars)``
+const Name = styled(NavBars)`
+  color: white;
+`
 
 class Header extends Component {
+  state = {
+    hide: true
+  }
+
+  handleMenuClick = (route) => {
+    this.setState({ hide: true });
+    this.props.history.push(`/${route}`);
+  }
+
   render() {
+
     return (
-      <StyledHeader>
-        <Name>MS</Name>
-        <NavBars className="fas fa-bars"></NavBars>
-      </StyledHeader>
+      <div>
+        <StyledHeader>
+          <Name onClick={ () => this.props.history.push('/') }>Megan Shepherd</Name>
+          <NavBars 
+            onClick={ () => this.setState({ hide: !this.state.hide }) }
+            className="fas fa-bars"
+            inputColor={ this.state.hide ? 'white' : 'rgb(73, 73, 73)' }>
+          </NavBars>
+        </StyledHeader>
+        { this.state.hide ? null : 
+        <Drop>
+          <MenuItems onClick={ () => this.handleMenuClick('about') }>About</MenuItems>
+          <MenuItems onClick={ () => this.handleMenuClick('skills') }>Skills</MenuItems>
+          <MenuItems onClick={ () => this.handleMenuClick('projects') }>Projects</MenuItems>
+          <MenuItems onClick={ () => this.handleMenuClick('contact') }>Contact</MenuItems>
+        </Drop> }
+      </div>
     );
   }
 }
 
-export default Header;
+export default withRouter(Header);
