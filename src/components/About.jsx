f5a57754f9b5b139579856701110c6cccda9bf6f
mobydickens <steadmegan@gmail.com>
2019-02-18 17:28:53 -0700

    More styling with the photos on the about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,8 @@ import view from '../pictures/view.jpg'
 
 const Container = styled.div`
   background-color: white;
-  background-image: -webkit-linear-gradient(30deg, #013A6B 50%, white 50%);
-  min-height: 500px;
-`
-const AboutOuter = styled.div`
-//   width: 0;
-//   height: 0;
-//   border-top: 100px solid gray;
-//   border-right: 100px solid white;
+  background-image: -webkit-linear-gradient(50deg, #52afff 50%, white 50%);
+  min-height: 100px;
 `
 const AboutTitle = styled.h1`
   font-size: 40px;
@@ -40,11 +34,9 @@ const SocialIcons = styled.a`
   margin: 10px;
   cursor: pointer;
 `
-const DarkBox = styled.div`
+const Box = styled.div`
   display: flex;
-  padding-top: 20px;
   justify-content: center;
-  // background-color: rgb(73, 73, 73);
   height: 100px;
 
 `
@@ -53,7 +45,7 @@ const Profile = styled.div`
   background-color: rgb(73, 73, 73);
   margin: 0 auto;
   margin-top: 40px;
-  margin-bottom: 40px;
+  margin-bottom: 20px;
   height: 270px;
   width: 270px;
   border-radius: 50%;
@@ -64,47 +56,46 @@ const Picture = styled.img`
   border-radius: 50%;
   object-fit: contain;
 `
-const RandomFacts = styled.div`
-  // background-color: rgb(73, 73, 73);
-`
-const Facts = styled(Description)`
+const RandomFacts = styled(SubTitle)`
 `
 const Pictures = styled.div`
-  background-color: rgb(73, 73, 73);
+  display: flex;
+  overflow: scroll;
 `
 
 const Covers = styled.img`
-  width: 25%;
+  margin: 5px;
+  height: 200px;
+  border: 5px solid white;
+
 `
 
 class About extends Component {
   render() {
     return (
       <Container>
-        <AboutOuter>
-          <AboutTitle>About me</AboutTitle>
-          <SubTitle>I'm a full stack web developer based near Salt Lake City, Utah.</SubTitle>
-          <Description>I love solving problems and am passionate about learning new skills. My favorite hobby when I'm not working is to settle down with a good book of any genre. I also enjoy staying active by backpacking and hiking.</Description>
-            <Profile>
-              <Picture src={me}></Picture>
-            </Profile>
-          <RandomFacts>
-            <Facts>Some other facts about me...</Facts>
-          </RandomFacts>
-          <Pictures>
-            <Covers src={flying} alt="the book of flying"></Covers>
-            <Covers src={pale} alt="pale fire"></Covers>
-            <Covers src={view} alt="a room with a view"></Covers>
-            <Covers src={howl} alt="howl's moving castle"></Covers>
-          </Pictures>
-          <DarkBox>
-            <SocialLinks>
-              <SocialIcons href='https://www.linkedin.com/in/megandanielleshepherd/'><i className="fab fa-linkedin"></i></SocialIcons>
-              <SocialIcons href='https://github.com/mobydickens'><i className="fab fa-github"></i></SocialIcons>
-              <SocialIcons href='https://www.facebook.com/megan.stead.33'><i className="fab fa-facebook"></i></SocialIcons>
-            </SocialLinks>
-          </DarkBox>
-        </AboutOuter>
+        <AboutTitle>About me</AboutTitle>
+        <SubTitle>I'm a full stack web developer based near Salt Lake City, Utah.</SubTitle>
+        <Description>I love solving problems and am passionate about learning new skills. My favorite hobby when I'm not working is to settle down with a good book of any genre. I also enjoy staying active by backpacking and hiking.</Description>
+        <Profile>
+          <Picture src={me}></Picture>
+        </Profile>
+        <Box>
+          <SocialLinks>
+            <SocialIcons href='https://www.linkedin.com/in/megandanielleshepherd/'><i className="fab fa-linkedin"></i></SocialIcons>
+            <SocialIcons href='https://github.com/mobydickens'><i className="fab fa-github"></i></SocialIcons>
+            <SocialIcons href='https://www.facebook.com/megan.stead.33'><i className="fab fa-facebook"></i></SocialIcons>
+          </SocialLinks>
+        </Box>
+        <RandomFacts>
+          Some of my favorite books
+        </RandomFacts>
+        <Pictures>
+          <Covers src={flying} alt="the book of flying"></Covers>
+          <Covers src={pale} alt="pale fire"></Covers>
+          <Covers src={view} alt="a room with a view"></Covers>
+          <Covers src={howl} alt="howl's moving castle"></Covers>
+        </Pictures>
       </Container>
     );
   }
